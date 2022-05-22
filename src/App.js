5e90a08f4ae5772fbd797f22e575c34bb08cc8ce
Dostonbek <mardonovdoston69@gmail.com>
2022-05-22 10:37:37 +0300

    dvnd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import React, { useState } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const [count, setCount] = useState(0);
+
+  function inc() {
+    setCount((prev) => prev + 1);
+  }
+  function dec() {
+    setCount((prev) => prev - 1);
+  }
+  return (
+    <>
+      <h3>Count:{count}</h3>
+      <button onClick={inc} className="btn btn-success">
+        INCR
+      </button>
+      <button onClick={dec} className="btn btn-danger">
+        DECR
+      </button>
+    </>
+  );
 }
 
 export default App;
